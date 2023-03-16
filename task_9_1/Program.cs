23087702ffef900d4129550748e711c404318dc9
Alex Mark <alexvmar@yandex.ru>
2023-03-16 16:24:31 +0300

    created files homework9

---
+++ task_9_1/Program.cs
@@ -0,0 +1,25 @@
+﻿/*  Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"  */
+
+using System;
+using static System.Console;
+Clear();
+
+int Prompt(string message)
+{
+    Write(message);
+    int num_result = int.Parse(ReadLine()!);
+    return num_result;
+}
+
+string PrintStringNum(int num, int firstnum)
+{
+    if (firstnum == num) return firstnum.ToString();
+    return (firstnum + " " + PrintStringNum(num, firstnum - 1));
+}
+
+int m = 1;
+int n = Prompt("Введите число N: ");
+
+WriteLine($"Натуральные числа в промежутке от {n} до 1: {PrintStringNum(m, n)}");
---
+++ task_9_2/Program.cs
@@ -0,0 +1,35 @@
+﻿/*
+Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+
+*/
+
+//Задайте значения M и N. Напишите программу, которая найдёт сумму 
+//натуральных элементов в промежутке от M до N.
+
+
+using System;
+using static System.Console;
+Clear();
+
+int Prompt(string message)
+{
+    Write(message);
+    int num_result = int.Parse(ReadLine()!);
+    return num_result;
+}
+
+int SumNumbers(int start, int end)
+{
+    if (start == end) return start;
+    return (start + SumNumbers(start + 1, end));
+}
+
+int m = Prompt("Введите число M: ");
+int n = Prompt("Введите число N: ");
+
+WriteLine($"Cумма натуральных элементов от {m} до {n} = {SumNumbers(m, n)}");
+
+
